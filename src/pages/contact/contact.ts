85b1649bb927d728625ed84de650936de45161e6
Tomasz Szymański <tszymanski@mir.local>
2018-09-24 10:18:27 +0200

    działa tworzenie i sprawdzanie katalogów w internalStorage

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+// import { NavController } from 'ionic-angular';
 import { Transfer, TransferObject } from '@ionic-native/transfer';
 import { File } from '@ionic-native/file';
 import { FilePath } from '@ionic-native/file-path';
-import { Platform } from 'ionic-angular';
+// import { Platform } from 'ionic-angular';
 
 @Component({
     selector: 'page-contact',
@@ -12,29 +12,29 @@ import { Platform } from 'ionic-angular';
 })
 export class ContactPage {
 
-    storageDirectory: string = '';
+    // storageDirectory: string = '';
 
-    constructor(public navCtrl: NavController, private filePath: FilePath, public platform: Platform, private transfer: Transfer, private file: File) {
+    // constructor(public navCtrl: NavController, private filePath: FilePath, public platform: Platform, private transfer: Transfer, private file: File) {
 
-        this.platform.ready().then(() => {
-            file.listDir(file.externalDataDirectory, '').then((result) => {
-                console.log(result);
-                /*result will have an array of file objects with 
-                file details or if its a directory*/
-                for (let file of result) {
-                    if (file.isDirectory == true && file.name != '.' && file.name != '..') {
-                        // Code if its a folder
-                    } else if (file.isFile == true) {
-                        // Code if its a file
-                        let name = file.name // File name
-                        let path = file.toURL // File path
-                        file.getMetadata(function (metadata) {
-                            let size = metadata.size; // Get file size
-                        })
-                    }
-                }
-            })
+    // this.platform.ready().then(() => {
+    //     file.listDir(file.externalDataDirectory, '').then((result) => {
+    //         console.log(result);
+    //         /*result will have an array of file objects with 
+    //         file details or if its a directory*/
+    //         for (let file of result) {
+    //             if (file.isDirectory == true && file.name != '.' && file.name != '..') {
+    //                 // Code if its a folder
+    //             } else if (file.isFile == true) {
+    //                 // Code if its a file
+    //                 let name = file.name // File name
+    //                 let path = file.toURL // File path
+    //                 file.getMetadata(function (metadata) {
+    //                     let size = metadata.size; // Get file size
+    //                 })
+    //             }
+    //         }
+    //     })
 
-        }
-    }
+    // }
+    // }
 }
--- src/pages/scan/scan.ts
+++ src/pages/scan/scan.ts
@@ -9,6 +9,10 @@ import { DetailsPage } from '../details/details';
 })
 export class ScanPage {
     tracks: any;
+    exist1: boolean;
+    exist2: boolean;
+    exist3: string;
+    exist4: any;
 
     constructor(
         public navCtrl: NavController,
@@ -16,18 +20,44 @@ export class ScanPage {
     ) { }
 
     ngOnInit() {
+        this.createDir();
         this.listFiles();
+        this.getFreeSpace();
+    }
+
+    createDir() {
+        this.file.createDir(this.file.externalRootDirectory, 'mydircordova', true);
     }
 
     listFiles() {
-        this.file.checkDir(this.file.dataDirectory, '../../app').then(_ => console.log('Directory exists')).catch(err => console.log('Directory doesn\'t exist'));
+        this.file.checkDir(this.file.externalRootDirectory, 'mydircordova').then(_ => {
+            console.log('Directory exists');
+            this.exist1 = true;
+        }
+        ).catch(err => {
+            console.log('Directory doesn\'t exist');
+            this.exist1 = false;
+        });
 
         this.file
-            .checkDir('www/assets/', 'data')
+            .checkDir(this.file.externalRootDirectory, 'mydircordove')
             .then(result => {
                 console.log(result);
+                this.exist2 = result;
             }).catch((error) => {
                 console.log('error ' + JSON.stringify(error));
+                this.exist3 = 'error ' + JSON.stringify(error)
+            });
+    }
+
+    getFreeSpace() {
+        this.file.getFreeDiskSpace()
+            .then(function (success) {
+                console.log(success);
+                this.exist4 = success;
+            }, function (error) {
+                console.log(error);
+                this.exist4 = error;
             });
     }
 
